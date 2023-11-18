0359f8d0ef90f1f840dcd1dfeac5765998432a2c
och-och <och.hco.och.hco@gmail.com>
2023-11-18 18:22:51 +0300

    Fix prop mutations

--- src/components/DotEditor.vue
+++ src/components/DotEditor.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 import NumberInput from "@/components/NumberInput.vue"
 
-const emit = defineEmits(["update:model-value", "delete"])
+const emit = defineEmits<{
+	(e: "update:model-value", dot: Dot): void
+	(e: "delete"): void
+}>()
+
 const { modelValue } = defineProps<{
 	modelValue: Dot
 }>()
+
+function update(dot: Dot) {
+	emit("update:model-value", dot)
+}
 </script>
 
 <template>
 	<div>
-		<NumberInput title="X" :min="-150" :max="150" v-model="modelValue.position.x" />
-		<NumberInput title="Y" :min="-150" :max="150" v-model="modelValue.position.y" />
-		<NumberInput title="Size" :max="150" v-model="modelValue.size" />
+		<NumberInput title="X" :min="-150" :max="150" :model-value="modelValue.position.x" @update:model-value="x => update({ ...modelValue, position: { ...modelValue.position, x } })" />
+		<NumberInput title="Y" :min="-150" :max="150" :model-value="modelValue.position.y" @update:model-value="y => update({ ...modelValue, position: { ...modelValue.position, y } })"/>
+		<NumberInput title="Size" :max="150" :model-value="modelValue.size" @update:model-value="size => update({ ...modelValue, size })" />
 		<p>
 			Mirror
-			<input type="checkbox" v-model="modelValue.mirrorX"> x
-			<input type="checkbox" v-model="modelValue.mirrorY"> y
+			<input type="checkbox" :value="modelValue.mirrorX" @input="update({ ...modelValue, mirrorX: ($event.target as HTMLInputElement)?.checked })"> x
+			<input type="checkbox" :value="modelValue.mirrorY" @input="update({ ...modelValue, mirrorY: ($event.target as HTMLInputElement)?.checked })"> y
 		</p>
 		<br />
 		<button type="button" @click="$emit('delete')">&Cross;</button>
